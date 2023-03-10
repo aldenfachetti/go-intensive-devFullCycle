935c3dfcad201538b51c29ad496ebbb2d7b8577f
aldenfachetti <aldenmerlin@gmail.com>
2023-03-10 08:00:08 +0100

    Kafka set up in project

---
+++ cmd/consumer/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/devfullcycle/gointensive/internal/infra/database"
+	"github.com/devfullcycle/gointensive/internal/usecase"
+	"github.com/devfullcycle/gointensive/pkg/kafka"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./orders.db")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close() // run everything and then run close
+
+	repository := database.NewOrderRepository(db)
+	usecase := usecase.CalculateFinalPrice{OrderRepository: repository}
+	msgChanKafka := make(chan *ckafka.Message)
+
+	topics := []string{"orders"}
+	servers := "host.docker.internal:9094"
+	go kafka.Consume(topics, servers, msgChanKafka)
+	kafkaworker(msgChanKafka, usecase)
+}
+
+func kafkaworker(msgChan chan *ckafka.Message, uc usecase.CalculateFinalPrice) {
+	for msg := range msgChan {
+		var OrderInputDTO usecase.OrderInputDTO
+		err := json.Unmarshal(msg.Value, &OrderInputDTO)
+		if err != nil {
+			panic(err)
+		}
+
+		outputDTO, err := uc.Execute(OrderInputDTO)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Kafka has processed order %s\n", outputDTO.ID)
+	}
+}
